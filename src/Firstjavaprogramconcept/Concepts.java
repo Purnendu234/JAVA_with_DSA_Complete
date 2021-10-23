7a7ddea9c70243e7377a817b70eed98607b0ef33
Purnendu <purnendu6299236620@example.com>
2021-10-23 15:31:21 +0530

    Initial commit

---
+++ src/Firstjavaprogramconcept/Concepts.java
@@ -0,0 +1,10 @@
+package Firstjavaprogramconcept;
+
+public class Concepts { // why here there is a term concepts working?//Answers: the file name we choose for coding the same name is placed for the public class so that evryone can acccess the same.
+
+	public static void main(String[] args) {// Just like in C++ the program starts from the main function that is "int main" similarly in JAVA the main function is Public static void main string[] args
+		System.out.println("Hello world");// TODO Auto-generated method stub
+
+	}
+
+}
